b484256fbd0e924f012ab1351f81dae1143e2049
Ajinkya <ajinkya.jagtap@vanderlande.com>
2018-04-18 00:19:48 +0530

    main class changed

--- weatherwiz/src/main/java/weather/wiz/App.java
+++ weatherwiz/src/main/java/weather/wiz/App.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
@@ -14,7 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAutoConfiguration
 @EnableJpaRepositories("weather.wiz.repository")
 @EntityScan("weather.wiz.entity")
-public class App 
+public class App extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
